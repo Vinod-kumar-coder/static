8837caac527ece6bf12dc572d6478d658a27c36f
Vinod-kumar-coder <vinodkumaryeddula24@gmail.com>
2026-07-08 11:49:36 +0530

    used if else statements in javasacript for changing colors

--- javascript.js
+++ javascript.js
@@ -7,16 +7,31 @@ function decrement(){
     let previousCounterValue=counterValueElement.textContent;
     let updatedCounterValue = parseInt(previousCounterValue) - 1;
     counterValueElement.textContent=updatedCounterValue;
+     if (updatedCounterValue<0){
+        counterValueElement.style.color="red";
+    }
+     else{
+        if (updatedCounterValue==0){
+            counterValueElement.style.color="black";
+        }
+     }
 }
 
 function increment(){
     let previousCounterValue=counterValueElement.textContent;
     let updatedCounterValue = parseInt(previousCounterValue) + 1;
     counterValueElement.textContent=updatedCounterValue;
+    if (updatedCounterValue>0){
+        counterValueElement.style.color="green";
+    }
+    else{
+        if (updatedCounterValue==0){
+            counterValueElement.style.color="black";
+        }
+     }
 }
 
 function reset(){
-
-    let updatedCounterValue = 0;
-    counterValueElement.textContent=updatedCounterValue;
+    counterValueElement.style.color="black";
+    counterValueElement.textContent=0;
 }
